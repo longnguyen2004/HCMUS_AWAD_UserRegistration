d7a8803881cae731a4ebe8d5187f919b0743b1e5
Long Nguyen <nguyen.long.908132@gmail.com>
2025-11-09 15:13:09 +0700

    Allow setting backend URL from env

--- app/frontend/src/lib/backend.ts
+++ app/frontend/src/lib/backend.ts
@@ -1,4 +1,4 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "backend";
 
-export const backend = treaty<App>("http://localhost:3000");
+export const backend = treaty<App>(import.meta.env.VITE_BACKEND_URL);
