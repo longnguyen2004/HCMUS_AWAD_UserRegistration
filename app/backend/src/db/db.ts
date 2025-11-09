57ce9251981899d6831697ee7564860c65a27bae
Long Nguyen <nguyen.long.908132@gmail.com>
2025-11-09 15:18:30 +0700

    Use connection string instead of separate parameters

--- app/backend/src/db/db.ts
+++ app/backend/src/db/db.ts
@@ -3,22 +3,11 @@ import { Kysely } from "kysely";
 import { PostgresJSDialect } from "kysely-postgres-js";
 import type { Database } from "./types.js";
 
-if (!process.env.DB_HOST)
-    throw new Error("DB host not specified");
-if (!process.env.DB_PORT)
-    throw new Error("DB port not specified");
-if (!process.env.DB_USER)
-    throw new Error("DB user not specified");
-if (!process.env.DB_PASS)
-    throw new Error("DB pass not specified");
+if (!process.env.DB_CONNECTION_STRING)
+    throw new Error("DB connection string not specified");
 
 export const db = new Kysely<Database>({
     dialect: new PostgresJSDialect({
-        postgres: postgres({
-            host: process.env.DB_HOST,
-            port: Number.parseInt(process.env.DB_PORT, 10),
-            user: process.env.DB_USER,
-            pass: process.env.DB_PASS
-        })
+        postgres: postgres(process.env.DB_CONNECTION_STRING)
     })
 });
