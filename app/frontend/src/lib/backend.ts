a06450bf74600a65c72aa674098d19e10c953b68
Long Nguyen <nguyen.long.908132@gmail.com>
2025-11-24 16:03:10 +0700

    Add credentials to all requests

--- app/frontend/src/lib/backend.ts
+++ app/frontend/src/lib/backend.ts
@@ -1,8 +1,13 @@
+import ky from "ky";
 import { treaty } from "@elysiajs/eden";
 import { createAuthClient } from "better-auth/react";
 import type { App } from "backend";
 
-export const backend = treaty<App>(import.meta.env.VITE_BACKEND_URL);
+const client = ky.create({
+  credentials: "include",
+  throwHttpErrors: false
+})
+export const backend = treaty<App>(import.meta.env.VITE_BACKEND_URL, { fetcher: client });
 export const backendAuth = createAuthClient({
   baseURL: import.meta.env.VITE_BACKEND_URL
 });
