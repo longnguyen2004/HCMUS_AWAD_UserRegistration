bc84af1dbc9487563d7f0e20565237a1b2507d26
Long Nguyen <nguyen.long.908132@gmail.com>
2025-12-06 21:18:39 +0700

    fix(be-db): association fields

--- app/backend/src/db/associations.ts
+++ app/backend/src/db/associations.ts
@@ -1,20 +1,20 @@
 import * as Models from "./models/index.js";
 
 export function setupAssociations() {
-  Models.BusStop.belongsTo(Models.City, { foreignKey: "cityId" });
-  Models.City.hasMany(Models.BusStop, { foreignKey: "cityId" });
+  Models.BusStop.belongsTo(Models.City, { foreignKey: "cityId", as: "city" });
+  Models.City.hasMany(Models.BusStop, { foreignKey: "cityId", as: "busStops" });
 
   Models.Seat.belongsTo(Models.Bus, { foreignKey: "busId" });
-  Models.Bus.hasMany(Models.Seat, { foreignKey: "busId" });
+  Models.Bus.hasMany(Models.Seat, { foreignKey: "busId", as: "bus" });
 
-  Models.Ticket.belongsTo(Models.Trip, { foreignKey: "tripId" });
-  Models.Ticket.belongsTo(Models.Seat, { foreignKey: "seatId" });
-  Models.Ticket.belongsTo(Models.User, { foreignKey: "userId" });
+  Models.Ticket.belongsTo(Models.Trip, { foreignKey: "tripId", as: "trip" });
+  Models.Ticket.belongsTo(Models.Seat, { foreignKey: "seatId", as: "seat" });
+  Models.Ticket.belongsTo(Models.User, { foreignKey: "userId", as: "user" });
 
-  Models.Trip.belongsTo(Models.Bus, { foreignKey: "busId" });
+  Models.Trip.belongsTo(Models.Bus, { foreignKey: "busId", as: "bus" });
 
-  Models.TripBusStop.belongsTo(Models.Trip, { foreignKey: "tripId" });
-  Models.Trip.hasMany(Models.TripBusStop, { foreignKey: "tripId" });
-  Models.TripBusStop.belongsTo(Models.BusStop, { foreignKey: "busId" });
-  Models.BusStop.hasMany(Models.TripBusStop, { foreignKey: "busId" });
+  Models.TripBusStop.belongsTo(Models.Trip, { foreignKey: "tripId", as: "trip" });
+  Models.Trip.hasMany(Models.TripBusStop, { foreignKey: "tripId", as: "tripBusStops" });
+  Models.TripBusStop.belongsTo(Models.BusStop, { foreignKey: "busStopId", as: "busStop" });
+  Models.BusStop.hasMany(Models.TripBusStop, { foreignKey: "busStopId", as: "tripBusStops" });
 }
